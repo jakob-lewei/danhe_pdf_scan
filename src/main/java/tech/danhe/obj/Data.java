f33a53f084f51ca273f7087f2b9d7c60b6e77a8e
Jakob He <jakob.he@ringcentral.com>
2019-10-16 17:15:03 +0800

    add translation

--- src/main/java/tech/danhe/obj/Data.java
+++ src/main/java/tech/danhe/obj/Data.java
@@ -1,9 +1,18 @@
 package tech.danhe.obj;
 
-import java.util.Objects;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
 public class Data {
 
+    private String b1;
+    private String c7_academic;
+    private String c9_sat_math;
+    private String c9_sat_reading;
     private String i2;
     private String c11_3_75_higher;
     private String c11_3_50_3_74;
@@ -12,91 +21,4 @@ public class Data {
     private String c11_2_50_2_99;
     private String c12;
 
-    public String getI2() {
-        return i2;
-    }
-
-    public void setI2(String i2) {
-        this.i2 = i2;
-    }
-
-    public String getC11_3_75_higher() {
-        return c11_3_75_higher;
-    }
-
-    public void setC11_3_75_higher(String c11_3_75_higher) {
-        this.c11_3_75_higher = c11_3_75_higher;
-    }
-
-    public String getC11_3_50_3_74() {
-        return c11_3_50_3_74;
-    }
-
-    public void setC11_3_50_3_74(String c11_3_50_3_74) {
-        this.c11_3_50_3_74 = c11_3_50_3_74;
-    }
-
-    public String getC11_3_25_3_49() {
-        return c11_3_25_3_49;
-    }
-
-    public void setC11_3_25_3_49(String c11_3_25_3_49) {
-        this.c11_3_25_3_49 = c11_3_25_3_49;
-    }
-
-    public String getC11_3_00_3_24() {
-        return c11_3_00_3_24;
-    }
-
-    public void setC11_3_00_3_24(String c11_3_00_3_24) {
-        this.c11_3_00_3_24 = c11_3_00_3_24;
-    }
-
-    public String getC11_2_50_2_99() {
-        return c11_2_50_2_99;
-    }
-
-    public void setC11_2_50_2_99(String c11_2_50_2_99) {
-        this.c11_2_50_2_99 = c11_2_50_2_99;
-    }
-
-    public String getC12() {
-        return c12;
-    }
-
-    public void setC12(String c12) {
-        this.c12 = c12;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Data data = (Data) o;
-        return Objects.equals(i2, data.i2) &&
-                Objects.equals(c11_3_75_higher, data.c11_3_75_higher) &&
-                Objects.equals(c11_3_50_3_74, data.c11_3_50_3_74) &&
-                Objects.equals(c11_3_25_3_49, data.c11_3_25_3_49) &&
-                Objects.equals(c11_3_00_3_24, data.c11_3_00_3_24) &&
-                Objects.equals(c11_2_50_2_99, data.c11_2_50_2_99) &&
-                Objects.equals(c12, data.c12);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(i2, c11_3_75_higher, c11_3_50_3_74, c11_3_25_3_49, c11_3_00_3_24, c11_2_50_2_99, c12);
-    }
-
-    @Override
-    public String toString() {
-        return "\n Data{" +
-                "i2='" + i2 + '\'' +
-                ", c11_3_75_higher='" + c11_3_75_higher + '\'' +
-                ", c11_3_50_3_74='" + c11_3_50_3_74 + '\'' +
-                ", c11_3_25_3_49='" + c11_3_25_3_49 + '\'' +
-                ", c11_3_00_3_24='" + c11_3_00_3_24 + '\'' +
-                ", c11_2_50_2_99='" + c11_2_50_2_99 + '\'' +
-                ", c12='" + c12 + '\'' +
-                '}';
-    }
 }
--- src/main/java/tech/danhe/service/LinesAnalyzeService.java
+++ src/main/java/tech/danhe/service/LinesAnalyzeService.java
@@ -5,9 +5,18 @@ import tech.danhe.obj.Data;
 import java.util.List;
 
 public class LinesAnalyzeService {
+    private static final String B1 = "B1";
+    private static final String TOTAL_ALL_UNDERGRADUATES = "Total all undergraduates";
+    private static final String C7 = "C7";
+    private static final String C7_ACADEMIC = "Academic";
+    private static final String C9 = "C9";
+    private static final String C9_SAT_READING = "SAT Evidence-Based Reading and Writing";
+    private static final String C9_SAT_MATH = "SAT Math";
     private static final String I2 = "I2";
+    private static final String I2_Fall = "Fall 2018 Student to Faculty ratio";
     private static final String C11 = "C11";
-    private static final String HIGHER = "higher";
+    private static final String AND_HIGHER = "and higher";
+    private static final String S_3_75 = "3.75";
     private static final String S_2_50 = "2.50";
     private static final String S_2_99 = "2.99";
     private static final String S_3_00 = "3.00";
@@ -22,25 +31,59 @@ public class LinesAnalyzeService {
 
         for (int i = 0; i < lines.size(); i++) {
             String line = lines.get(i);
-            if (line.contains(I2)) {
-                data.setI2(line);
+            if (line.contains(I2) && line.contains(I2_Fall)) {
+                String value = subString(line, I2, I2_Fall);
+                data.setI2(value == null ? data.getI2() : value);
             }
-            if (line.contains(C11) && line.contains(HIGHER)) {
-                data.setC11_3_75_higher(line.substring(line.lastIndexOf(HIGHER) + HIGHER.length()).trim());
+            if (line.contains(C7) && line.contains(C7_ACADEMIC)) {
+                String value = subString(line, C7, C7_ACADEMIC);
+                data.setC7_academic(value == null ? data.getC7_academic() : value);
+            }
+            if (line.contains(B1) && line.contains(TOTAL_ALL_UNDERGRADUATES)) {
+                String value = subString(line, B1, TOTAL_ALL_UNDERGRADUATES);
+                data.setB1(value == null ? data.getB1() : value);
+            }
+            if (line.contains(C9) && line.contains(C9_SAT_MATH)) {
+                String value = subString(line, C9, C9_SAT_MATH);
+                data.setC9_sat_math(value == null ? data.getC9_sat_math() : value);
+            }
+            if (line.contains(C9) && line.contains(C9_SAT_READING)) {
+                String value = subString(line, C9, C9_SAT_READING);
+                data.setC9_sat_reading(value == null ? data.getC9_sat_reading() : value);
+            }
+            if (line.contains(C11) && line.contains(S_3_75)) {
+                String value = subString(line, C11, S_3_75, AND_HIGHER);
+                data.setC11_3_75_higher(value == null ? data.getC11_3_75_higher() : value);
             }
             if (line.contains(C11) && line.contains(S_2_50) && line.contains(S_2_99)) {
-                data.setC11_2_50_2_99(line.substring(line.lastIndexOf(S_2_99) + S_2_99.length()).trim());
+                String value = subString(line, C11, S_2_50, S_2_99);
+                data.setC11_2_50_2_99(value == null ? data.getC11_2_50_2_99() : value);
             }
             if (line.contains(C11) && line.contains(S_3_00) && line.contains(S_3_24)) {
-                data.setC11_3_00_3_24(line.substring(line.lastIndexOf(S_3_24) + S_3_24.length()).trim());
+                String value = subString(line, C11, S_3_00, S_3_24);
+                data.setC11_3_00_3_24(value == null ? data.getC11_3_00_3_24() : value);
             }
             if (line.contains(C11) && line.contains(S_3_25) && line.contains(S_3_49)) {
-                data.setC11_3_25_3_49(line.substring(line.lastIndexOf(S_3_49) + S_3_49.length()).trim());
+                String value = subString(line, C11, S_3_25, S_3_49);
+                data.setC11_3_25_3_49(value == null ? data.getC11_3_25_3_49() : value);
             }
             if (line.contains(C11) && line.contains(S_3_50) && line.contains(S_3_74)) {
-                data.setC11_3_50_3_74(line.substring(line.lastIndexOf(S_3_74) + S_3_74.length()).trim());
+                String value = subString(line, C11, S_3_50, S_3_74);
+                data.setC11_3_50_3_74(value == null ? data.getC11_3_50_3_74() : value);
             }
         }
         return data;
+    }
+
+    private String subString(String line, String... condition) {
+        boolean flag = true;
+        for (int i = 0; i < condition.length; i++) {
+            flag &= line.contains(condition[i]);
+        }
+        if (flag) {
+            return line.substring(line.lastIndexOf(condition[condition.length - 1]) + condition[condition.length - 1].length()).trim();
+        } else {
+            return null;
+        }
     }
 }
